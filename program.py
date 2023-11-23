a987fdc84d1f2af5dec099de3098a459b5962d2a
Yara <inteligentpie@gmail.com>
2023-11-23 10:58:06 +0300

    Again

--- program.py
+++ program.py
@@ -2,7 +2,9 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Second Commit')
-    print('Remote Repository)
+    print('Remote Repository')
+    print('Again')
+
 
 if __name__ == '__main__':
     main()
